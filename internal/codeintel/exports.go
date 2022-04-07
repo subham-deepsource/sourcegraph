f90a58c4c5bb2a60cee48707cb3cfc3d7fcef312
Eric Fritz <eric@eric-fritz.com>
2022-04-07 12:15:21 -0500

    WIP.

--- internal/codeintel/exports.go
+++ internal/codeintel/exports.go
@@ -1,7 +1,16 @@
 package codeintel
 
-import "github.com/sourcegraph/sourcegraph/internal/codeintel/dependencies"
+import (
+	"github.com/sourcegraph/sourcegraph/internal/codeintel/dependencies"
+	"github.com/sourcegraph/sourcegraph/internal/codeintel/symbols"
+)
 
-type DependencyService dependencies.Service
+type (
+	DependencyService dependencies.Service
+	SymbolsService    symbols.Service
+)
 
-var GetDependenciesService = dependencies.GetService
+var (
+	GetDependenciesService = dependencies.GetService
+	GetSymbolsService      = symbols.GetService
+)
