316ff2a6c309d260832ca572b0a45d068ff4b2d2
Justin Boyson <justin.boyson@sourcegraph.com>
2022-04-07 20:09:21 -0500

    remove unused modal

--- client/web/src/enterprise/insights/components/insights-view-grid/components/backend-insight/BackendInsight.tsx
+++ client/web/src/enterprise/insights/components/insights-view-grid/components/backend-insight/BackendInsight.tsx
@@ -4,7 +4,6 @@ import classNames from 'classnames'
 import { useMergeRefs } from 'use-callback-ref'
 
 import { asError, isErrorLike } from '@sourcegraph/common'
-import { ConfirmationModal } from '@sourcegraph/shared/src/components/ConfirmationModal'
 import { TelemetryProps } from '@sourcegraph/shared/src/telemetry/telemetryService'
 import { useDebounce, Alert } from '@sourcegraph/wildcard'
 
@@ -47,8 +46,6 @@ export const BackendInsightView: React.FunctionComponent<BackendInsightProps> = 
 
     const { dashboard } = useContext(DashboardInsightsContext)
     const { getBackendInsightData, createInsight, updateInsight } = useContext(CodeInsightsBackendContext)
-
-    const [showDeleteConfirm, setShowDeleteConfirm] = useState(false)
 
     // Visual line chart settings
     const [zeroYAxisMin, setZeroYAxisMin] = useState(false)
@@ -194,17 +191,6 @@ export const BackendInsightView: React.FunctionComponent<BackendInsightProps> = 
             ) : (
                 data && (
                     <LineChartSettingsContext.Provider value={{ zeroYAxisMin }}>
-                        <ConfirmationModal
-                            showModal={showDeleteConfirm}
-                            handleCancel={() => setShowDeleteConfirm(false)}
-                            handleConfirmation={() => handleDelete(insight)}
-                            header="Delete Insight?"
-                            message={
-                                <>
-                                    Are you sure you want to delete the insight <strong>{insight.title}</strong>?
-                                </>
-                            }
-                        />
                         <View.Content
                             content={data.view.content}
                             alert={
